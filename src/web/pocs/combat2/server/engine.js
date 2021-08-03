d88b7f8383da01edc37c3831f82331e7a6967924
John <john.pavek@blendinteractive.com>
2021-08-02 22:53:07 -0500

    Pretty cool progress

--- src/web/pocs/combat2/server/engine.js
+++ src/web/pocs/combat2/server/engine.js
@@ -17,16 +17,26 @@ var middleware = require("socketio-wildcard")();
 io.use(middleware);
 
 let CONNECTIONS = {};
+let GAMES = {};
 
 const EVENTS = {
   connect: "connection",
   disconnect: "disconnect",
   updateCharacter: "updateCharacter",
+  commitCharacter: "commitCharacter",
 };
 
 io.sockets.on(EVENTS.connect, function (socket) {
   console.log(EVENTS.connect, socket.id);
   CONNECTIONS[socket.id] = socket;
+
+  // TODO Make rooms later
+
+  GAMES[socket.id] = new Game();
+  var player = new Entity();
+  player.components.push(new HealthComponent(100));
+
+  GAMES[socket.id].entities.push(player);
 
   socket.on(EVENTS.disconnect, function () {
     console.log("Disconnected", socket.id);
@@ -34,37 +44,145 @@ io.sockets.on(EVENTS.connect, function (socket) {
   });
 
   socket.on(EVENTS.updateCharacter, (data) => {
+    var player = GAMES[socket.id].entities.find((x) => (x.id = socket.id));
     console.log("updateCharacter", data);
-    CONNECTIONS[socket.id].gameData = data;
-    updateAll();
+    Object.keys(data).forEach((key) => {
+      switch (key) {
+        case "background":
+          player.addOrUpdateComponent(new Background(data[key]));
+          break;
+        case "class":
+          player.addOrUpdateComponent(new CharacterClass(data[key]));
+          break;
+        case "weapon":
+          var equipment = new Equipment();
+          equipment.Slots["MainHand"] = data[key];
+          player.addOrUpdateComponent(equipment);
+          break;
+      }
+    });
+
+    player.components.push();
+    updateAll(socket);
   });
 
-  updateAll();
+  socket.on(EVENTS.commitCharacter, (data) => {
+    CONNECTIONS[socket.id].gameData = updateAll(socket);
+  });
+
+  updateAll(socket);
 });
 
-function updateAll() {
+function updateAll(socket) {
+  GAMES[socket.id].updateState();
   Object.keys(CONNECTIONS).forEach((conn) => {
     CONNECTIONS[conn].emit(
       "update",
-      Object.keys(CONNECTIONS).map((x) => CONNECTIONS[x]?.gameData)
+      GAMES[socket.id].entities.map((x) =>
+        x.components.map((y) => y.displayForPlayer())
+      )
     );
   });
 }
 
-class GameEngine {
-  constructor() {}
+class Game {
+  entities = [];
+  systems = [];
+  constructor() {
+    /// Inject all Systems;
+    this.systems.push(new HealthSystem());
+  }
 
-  updateState() {}
-}
-
-class Entity {
-  components = [];
-
-  constructor(components) {
-    this.components = components;
+  updateState() {
+    this.systems.forEach((system) => {
+      this.entities.forEach(system.Tick);
+    });
   }
 }
 
-class Component {}
+class Entity {
+  constructor(id) {
+    this.components = [];
+    this.id = id ?? parseInt(Math.random().toString().substr(2));
+  }
+  addOrUpdateComponent(component) {
+    var existingComponent = this.components.find(
+      (x) => component.constructor.name == x.constructor.name
+    );
 
-class System {}
+    if (!existingComponent) {
+      this.components.push(component);
+    } else {
+    }
+  }
+}
+
+class Component {
+  displayForPlayer() {
+    throw "Implement me";
+  }
+}
+
+class System {
+  Tick() {
+    throw "Implement me";
+  }
+}
+
+class HealthSystem extends System {
+  Tick(component) {
+    component.CurrentHealth += component.RegenerationRate;
+    if (component.CurrentHealth > component.Max) {
+      component.CurrentHealth = component.max;
+    }
+  }
+}
+
+class HealthComponent extends Component {
+  constructor(max) {
+    super();
+
+    this.Max = max || 10;
+    this.CurrentHealth = this.Max;
+    this.RegenerationRate = 1;
+  }
+  displayForPlayer() {
+    return `Health: ${this.CurrentHealth} / ${this.Max}`;
+  }
+}
+
+class CharacterClass extends Component {
+  constructor(name) {
+    super();
+
+    this.Name = name ?? "None";
+  }
+  displayForPlayer() {
+    return `Class: ${this.Name}`;
+  }
+}
+
+class Background extends Component {
+  constructor(name) {
+    super();
+
+    this.Name = name || "None";
+  }
+  displayForPlayer() {
+    return `Background: ${this.Name}`;
+  }
+}
+class Equipment extends Component {
+  constructor() {
+    super();
+
+    this.Slots = {
+      MainHand: "",
+      Chest: "",
+    };
+  }
+  displayForPlayer() {
+    var gear = Object.keys(this.Slots).map((x) => `${x}: ${this.Slots[x]}`);
+    return gear.join("\n");
+  }
+}
--- src/web/pocs/combat2/web/js/game.js
+++ src/web/pocs/combat2/web/js/game.js
@@ -1,18 +1,41 @@
 var socket = io();
 
+const EVENTS = {
+  connect: "connection",
+  disconnect: "disconnect",
+  updateCharacter: "updateCharacter",
+  commitCharacter: "commitCharacter",
+};
+
 socket.on("update", function (data) {
   var connections = document.querySelector("#activeConnections");
+  console.log(data);
   connections.textContent = "";
-  data.forEach((element) => {
-    var li = document.createElement("li");
-    li.textContent = element;
-    connections.appendChild(li);
+  data
+    .filter((x) => x)
+    .forEach((element) => {
+      var li = document.createElement("li");
+      li.innerHTML = Object.keys(element)
+        .map((key) => `${key}: ${element[key]}`)
+        .join("<br>");
+      connections.appendChild(li);
+    });
+});
+
+document.querySelectorAll("input").forEach((x) => {
+  x.addEventListener("blur", (e) => {
+    var updateVals = {};
+    updateVals[e.target.name] = e.target.value;
+    socket.emit("updateCharacter", updateVals);
   });
 });
 
-document
-  .querySelector("button[data-class]")
-  .addEventListener("click", function (e) {
-    var name = document.querySelector('input[name="name"]').value;
-    socket.emit("updateCharacter", {class: e.target.dataset.class, name});
-  });
+document.querySelectorAll("button").forEach((x) =>
+  x.addEventListener("click", function (e) {
+    if (e.target.dataset.done) {
+      socket.emit(EVENTS.commitCharacter);
+    } else {
+      socket.emit(EVENTS.updateCharacter, e.target.dataset);
+    }
+  })
+);

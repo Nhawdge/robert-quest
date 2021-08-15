5fcb73cca0d8eb579a5bedc0a1e56daef671efee
John Pavek <JohnQPavek@gmail.com>
2021-08-14 21:28:44 -0500

    Map work

--- src/server/components/mapComponent.js
+++ src/server/components/mapComponent.js
@@ -5,41 +5,48 @@ export default class Map extends Component {
     super();
   }
   displayForPlayer() {
-    return `    <svg viewBox="0 0 1000 1000" width=800px>
+    return `<svg viewBox="0 0 1000 1000" width=800px>
     <style>
         .small {
-            font: italic 12px sans-serif;
+            font: italic 29px sans-serif;
             padding-left: 2px;
         }
+        .destination {
+            color:blue;
+        }
+        .destination:hover {
+            border: solid green 2px;
+            color:green;
+        }
     </style>
-    <g id="village">
+    <g class="destination" id="village">
         <image x="470" y="160" width="80" height="80" href="/icons/delapouite/village.svg" />
-        //<circle cx="510" cy="200" r="3"></circle>
+        <!-- <circle cx="510" cy="200" r="3"></circle> -->
         <text class="small" x="540" y="200">Town</text>
     </g>
-    <g id="forest">
+    <g  class="destination" id="forest">
         <image x="50" y="90" width="80" height="80" href="/icons/delapouite/forest.svg" />
-        //<circle cx="90" cy="130" r="3"></circle>
+        <!--  <circle cx="90" cy="130" r="3"></circle> -->
         <text class="small" x="130" y="130">Forest</text>
     </g>
-    <g id="cave">
+    <g class="destination" id="cave">
         <image x="330" y="320" width="80" height="80" href="/icons/delapouite/cave-entrance.svg" />
-        //<circle cx="370" cy="360" r="3"></circle>
+        <!--  <circle cx="370" cy="360" r="3"></circle> -->
         <text class="small" x="410" y="360">Cave</text>
     </g>
-    <g id="plains">
+    <g  class="destination" id="plains">
         <image x="300" y="560" width="80" height="80" href="/icons/delapouite/grass.svg" />
-        //<circle cx="340" cy="600" r="3"></circle>
+        <!--  <circle cx="340" cy="600" r="3"></circle> -->
         <text class="small" x="380" y="600">Plains</text>
     </g>
-    <g id="mountain">
+    <g class="destination" id="mountain">
         <image x="430" y="880" width="80" height="80" href="/icons/delapouite/mountain-road.svg" />
-        //<circle cx="470" cy="920" r="3"></circle>
+        <!-- <circle cx="470" cy="920" r="3"></circle> -->
         <text class="small" x="500" y="920">Mountain</text>
     </g>
-    <g id="seaport">
+    <g class="destination" id="seaport">
         <image x="810" y="220" width="80" height="80" href="/icons/delapouite/ship-bow.svg" />
-        //<circle cx="850" cy="260" r="3"></circle>
+        <!--  <circle cx="850" cy="260" r="3"></circle> -->
         <text class="small" x="890" y="260">Seaport</text>
     </g>
     <path stroke="black" fill="none" d="
